7b043b2282675ec8a05d799d9ea5d73d2a3d4f00
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2025-02-17 11:33:26 +0100

    Datasets were not saving acquisition mode, fixed

--- src/backend/datasets/views.py
+++ src/backend/datasets/views.py
@@ -1439,7 +1439,9 @@ def update_msacq(dset, data):
     if data['operator_id'] != dset.operatordataset.operator_id:
         dset.operatordataset.operator_id = data['operator_id']
         dset.operatordataset.save()
-    models.AcquisistionModeDataset.objects.filter(dataset=dset).update(acqmode=data['acqmode'])
+    # Just update is not enough since there may be datasets without acq mode
+    models.AcquisistionModeDataset.objects.update_or_create(dataset=dset,
+            defaults={'acqmode': data['acqmode']})
     if not hasattr(dset, 'reversephasedataset'):
         if data['rp_length']:
             models.ReversePhaseDataset.objects.create(dataset_id=dset.id,
@@ -1552,6 +1554,7 @@ def save_ms_acquisition(request):
     dset = models.Dataset.objects.filter(pk=data['dataset_id']).get()
     if hasattr(dset, 'operatordataset'):
         return update_msacq(dset, data)
+    models.AcquisistionModeDataset.objects.create(dataset=dset, acqmode=data['acqmode'])
     models.OperatorDataset.objects.create(dataset=dset, operator_id=data['operator_id'])
     save_admin_defined_params(data, dset_id)
     set_component_state(dset_id, models.DatasetUIComponent.ACQUISITION, models.DCStates.OK)
