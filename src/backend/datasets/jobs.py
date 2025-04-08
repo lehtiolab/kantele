b338920fa36805f86725c58c7f723ae32c28ef9b
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2025-04-08 16:42:36 +0200

    Trying to fix prod bugs pt3

--- src/backend/datasets/jobs.py
+++ src/backend/datasets/jobs.py
@@ -355,7 +355,7 @@ def get_or_create_mzmlentry(fn, pwiz, refined, servershare_id, path, mzmlfilenam
     '''This also resets the path of the mzML file in case it's deleted'''
     new_md5 = f'mzml_{fn.sfile.rawfile.source_md5[5:]}'
     mzsf, cr = StoredFile.objects.get_or_create(mzmlfile__pwiz=pwiz, mzmlfile__refined=refined,
-            rawfile_id=fn.rawfile_id, filetype_id=fn.sfile.filetype_id, defaults={'md5': new_md5,
+            rawfile_id=fn.sfile.rawfile_id, filetype_id=fn.sfile.filetype_id, defaults={'md5': new_md5,
                 'filename': mzmlfilename})
     sfl, _ = StoredFileLoc.objects.get_or_create(sfile=mzsf, defaults={'servershare_id': servershare_id, 'path': path})
     if cr:
