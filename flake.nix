7b9d30a09adc0534bb429c901c228259cfdd2314
Patrick CN Martin <pcnmartin@gmail.com>
2026-04-29 15:06:57 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Kantele Dev Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Backend & Tooling
+            python311
+            uv
+            # Matches Kantele README reqs
+            nodejs_22
+            nodePackages.npm
+            
+            # Infrastructure
+            docker
+            docker-compose
+            postgresql_18 # For local CLI access to the DB container
+            
+            # Quality of Life
+            git
+            pre-commit
+            ruff # Fast Python linting for the Django code
+          ];
+
+          shellHook = ''
+            echo "--- Kantele Development Environment ---"
+          '';
+        };
+      });
+}
